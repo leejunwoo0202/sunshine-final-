12ccd345dc4272232f5afce97ac52d6214acc0f3
82104 <82104@DESKTOP-CSU4KU7>
2021-03-18 10:57:23 +0900

    수정해야함

--- sunshine/src/main/java/sunshine/mapper/ticket/TicketMapper.java
+++ sunshine/src/main/java/sunshine/mapper/ticket/TicketMapper.java
@@ -1,12 +1,12 @@
-package sunshine.mapper.ticket;
-
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
-
-import sunshine.ski.DTO.TicketDTO;
-
-@Component
-@Repository
-public interface TicketMapper {
-	public String ticketIssue(TicketDTO ticketDTO);
-}
+//package sunshine.mapper.ticket;
+//
+//import org.springframework.stereotype.Component;
+//import org.springframework.stereotype.Repository;
+//
+//import sunshine.ski.DTO.TicketDTO;
+//
+//@Component
+//@Repository
+//public interface TicketMapper {
+//	public String ticketIssue(TicketDTO ticketDTO);
+//}
--- sunshine/src/main/java/sunshine/service/ski/TicketService.java
+++ sunshine/src/main/java/sunshine/service/ski/TicketService.java
@@ -1,22 +1,22 @@
-package sunshine.service.ski;
-
-import sunshine.command.TicketCommand;
-import sunshine.ski.DTO.TicketDTO;
-
-public class TicketService {
-	public void ticketIssue(TicketCommand ticketCommand) {
-		TicketDTO dto = new TicketDTO();
-		
-		//티켓번호
-		dto.setTicketNo(ticketCommand.getTicketNo());
-		//티켓종류
-		dto.setTicketKind(ticketCommand.getTicketKind());
-		//티켓가격
-		dto.setTicketPrice(ticketCommand.getTicketPrice());
-		//구분대상
-		dto.setDivPeople(ticketCommand.getDivPeople());
-		//이용기간
-		dto.setValidity(ticketCommand.getValidity());
-
-	}
-}
+//package sunshine.service.ski;
+//
+//import sunshine.command.TicketCommand;
+//import sunshine.ski.DTO.TicketDTO;
+//
+//public class TicketService {
+//	public void ticketIssue(TicketCommand ticketCommand) {
+//		TicketDTO dto = new TicketDTO();
+//		
+//		//티켓번호
+//		dto.setTicketNo(ticketCommand.getTicketNo());
+//		//티켓종류
+//		dto.setTicketKind(ticketCommand.getTicketKind());
+//		//티켓가격
+//		dto.setTicketPrice(ticketCommand.getTicketPrice());
+//		//구분대상
+//		dto.setDivPeople(ticketCommand.getDivPeople());
+//		//이용기간
+//		dto.setValidity(ticketCommand.getValidity());
+//
+//	}
+//}
