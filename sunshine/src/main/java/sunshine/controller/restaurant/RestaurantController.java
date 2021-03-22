32b385aa83eb360d83da5301e87ef812fb9d8071
DESKTOP-78MNPFP\lalal <lalalayu87@gmail.com>
2021-03-22 12:20:34 +0900

    레스토랑

--- sunshine/src/main/java/sunshine/controller/restaurant/RestaurantController.java
+++ sunshine/src/main/java/sunshine/controller/restaurant/RestaurantController.java
@@ -1,30 +1,30 @@
-package sunshine.controller;
+package sunshine.controller.restaurant;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(value = "restaurant")
+
 public class RestaurantController {
 	@RequestMapping(value = "restaurantMain")
 	public String restaurantMain() {
 		return "restaurant/index";
 	}
-	
+
 	@RequestMapping(value = "restaurantBookMg")
 	public String restaurantBookMg() {
 		return "restaurant/restaurantBookMg";
 	}
-	
+
 	@RequestMapping(value = "restaurantMenuMg")
 	public String restaurantMenuMg() {
 		return "restaurant/restaurantMenuMg";
 	}
-	
+
 	@RequestMapping(value = "restaurantRsMg")
 	public String restaurantRsMg() {
 		return "restaurant/restaurantRsMg";
 	}
-
 
 }
---
+++ sunshine/src/main/webapp/WEB-INF/view/restaurant/restaurantRsMg.jsp
@@ -0,0 +1,33 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>restaurantRsMg 룸서비스관리</title>
+</head>
+<body>
+<h1>레스토랑 룸서비스 관리</h1>
+<br>
+<table border="1">
+<tr>
+<th>룸서비스번호</th>
+<th>객실번호</th>
+<th>메뉴번호</th>
+<th>접수시간</th>
+<th>인원</th>
+<th>비고</th>
+</tr>
+<tr>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+</tr>
+</table>
+<input type="submit" value="등록">
+<input type="button"  value ="뒤로" onclick="javascript:location.href='../restaurant/restaurantMain'"/>  
+</body>
+</html>
